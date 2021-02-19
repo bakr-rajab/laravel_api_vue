0048437022f30f219d908cf89526aa84c0894139
bakr-rajab <abobakr.r.bakry@gmail.com>
2021-02-19 03:13:32 +0200

    setup laravel:7 and vue js

---
+++ resources/js/app.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import routes from './routes';
+
+
+Vue.use(VueRouter);
+
+// require('./bootstrap');
+
+// window.Vue = require('vue');
+
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+const app = new Vue({
+    el: '#app',
+
+
+    router: new VueRouter(routes)
+});
